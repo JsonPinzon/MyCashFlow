4d598059d46b73f0c06d522a603fedeb5c3bde1c
JJBC04 <04juanjoseb@gmail.com>
2024-11-19 17:58:41 -0500

    Add pagina de perfil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,32 +4,37 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./pages/inicio/inicio.module').then(m => m.InicioPageModule)
+    //loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: 'inicio',
-    loadChildren: () => import('./pages/inicio/inicio.module').then( m => m.InicioPageModule)
+    loadChildren: () => import('./pages/inicio/inicio.module').then(m => m.InicioPageModule)
   },
   {
     path: 'registro-gastos',
-    loadChildren: () => import('./pages/registro-gastos/registro-gastos.module').then( m => m.RegistroGastosPageModule)
+    loadChildren: () => import('./pages/registro-gastos/registro-gastos.module').then(m => m.RegistroGastosPageModule)
   },
   {
     path: 'reporte',
-    loadChildren: () => import('./pages/reporte/reporte.module').then( m => m.ReportePageModule)
+    loadChildren: () => import('./pages/reporte/reporte.module').then(m => m.ReportePageModule)
   },
   {
     path: 'objetivos',
-    loadChildren: () => import('./pages/objetivos/objetivos.module').then( m => m.ObjetivosPageModule)
+    loadChildren: () => import('./pages/objetivos/objetivos.module').then(m => m.ObjetivosPageModule)
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+    loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterPageModule)
+  },  {
+    path: 'perfil',
+    loadChildren: () => import('./pages/perfil/perfil.module').then( m => m.PerfilPageModule)
   }
+
 ];
 @NgModule({
   imports: [
@@ -37,4 +42,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
